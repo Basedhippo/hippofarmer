bd3dc878c865df7f26ef0f1b44e56e44bfdcf330
basedhippo <basedhippo@proton.me>
2024-10-07 17:07:23 +0000

    d7added new metrics

--- contracts/HippoBreeds.sol
+++ contracts/HippoBreeds.sol
@@ -10,13 +10,20 @@ import "./Base64.sol";
 import "./IDungToken.sol"; // Interface for DUNG token
 
 /// @title HippoBreeds
+<<<<<<< HEAD
 /// @notice A contract to mint and breed Legendary Hippo NFTs, with special steroid-boosted Hippo>
 /// @dev Inherits from TRC721, ITRC721Metadata, Ownable, and ReentrancyGuard.
 contract HippoBreeds is TRC21, ITRC721Metadata, Ownable, ReentrancyGuard {
 
+=======
+/// @notice A contract to mint and breed Legendary Hippo NFTs, with special steroid-boosted Hippos.
+/// @dev Inherits from TRC721, ITRC721Metadata, Ownable, and ReentrancyGuard.
+contract HippoBreeds is TRC21, ITRC721Metadata, Ownable, ReentrancyGuard {
+>>>>>>> 221e00f (added new metrics)
     using Strings for uint256;
     using Counters for Counters.Counter;
 
+   
     Counters.Counter private _tokenIdCounter;
 
     // Interface for the $DUNG token
